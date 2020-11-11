eccdc1dda07862e3b2cc4e788e28b2dacf0bb272
programacionrichard <programacionrichardg91@gmail.com>
2020-11-11 17:19:28 -0300

    mi nombre agregado

--- app.js
+++ app.js
@@ -6,20 +6,37 @@ const app = express()
 app.get('/', (req, res) => {
     
     res.sendFile(__dirname + '/mipagina.html')
-})
+});
 
 app.get('/muestra-imagen', (req, res) => {
     res.sendFile(__dirname + '/imagenpng.png')
 
-})
+});
 
 app.get('/acerca-de', (req, res,) => {
     res.send("acerca")
     
-})
+});
 
 app.get('/habilidades', (req, res) => {
     res.send("habilidades")
-})
+
+});
+
+app.get('/new', (req, res) => {
+    res.send("nueva pagina!!!!")
+
+});
+
+app.get('/anyname', (req, res) => {
+    res.send("<h1>RICHARD GARCIA/h1>")
+
+});
+
+
+
+   
+    
+
 
 app.listen(process.env.PORT || 3000)
